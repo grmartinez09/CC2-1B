589e91caa082f9086d99d19e622b92b408e91437
Gerryle R. Martinez <145944203+grmartinez09@users.noreply.github.com>
2023-09-27 12:16:29 -0400

    Add files via upload

---
+++ labact2-hello-world/helloworld.py
@@ -0,0 +1,19 @@
+# this is a sample code
+name = input("What is your name?")
+age = int(input("What is your age?"))
+
+age_input = input("What is your age?")
+
+age = int(age_input)
+
+age_next_year = age +1
+
+modulo = 5%3
+
+pi = 3.1416
+
+
+print(f"Hello, {name}")
+print(f"My age next year is, {(age+1)}")
+print(f"Hello," ,name, sep="%%%%")
+print(f"My age next year is {age_next_year}")
